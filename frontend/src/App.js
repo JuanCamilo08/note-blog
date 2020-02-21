c722a81ec3b38ba9ec64fb8918ce17f76c8504df
JuanCamilo08 <jcamilo@intellisys.com.do>
2020-02-21 14:25:04 -0400

    making the get request with axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return <div className="App">hello world</div>;
+class App extends React.Component {
+  state = {
+    notes: null
+  };
+
+  getData = async () => {
+    const notes = await axios.get('https://localhost:3000/api/note', {
+      headers: { 'Content-Type': 'application/json' }
+    });
+    this.setState({ notes });
+    console.log('here', notes.data);
+  };
+  async componentDidMount() {
+    await this.getData();
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className="search">
+          <input className="input" />
+          <button className="btn">agregar</button>
+        </div>
+        <div>{}</div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
