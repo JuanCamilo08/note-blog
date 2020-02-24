8ba13ac69f9b07aeddf94ababe9d06a00ea89e58
JuanCamilo08 <jcamilo@intellisys.com.do>
2020-02-24 13:20:28 -0400

    add the middleware cors to allow the app to consume the api

--- backend/startup/routes.js
+++ backend/startup/routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const noteRoutes = require('../routes/notes');
 const error = require('../middleware/error');
 
 module.exports = function(app) {
   app.use(express.json());
+  app.use(cors());
   app.use(morgan('dev'));
   app.use('/api/note', noteRoutes);
   app.use(error);
