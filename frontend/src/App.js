aecd1d7abbc1f6c79ed1e19882359d0efd02d755
JuanCamilo08 <jcamilo@intellisys.com.do>
2020-02-25 08:36:06 -0400

    show and add correctly the notes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,71 @@
 import React from 'react';
-import axios from 'axios';
+import axios from './utils/axios';
+import { fetchNotes, postNote } from './utils/api';
 
 class App extends React.Component {
   state = {
-    notes: null
+    content: '',
+    notes: null,
+    err: null
   };
 
-  getData = async () => {
-    const notes = await axios.get('https://localhost:3000/api/note', {
-      headers: { 'Content-Type': 'application/json' }
-    });
-    this.setState({ notes });
-    console.log('here', notes.data);
+  onClick = async event => {
+    event.preventDefault();
+
+    const { content } = this.state;
+
+    try {
+      const post = await postNote(content);
+
+      const data = await fetchNotes();
+
+      console.log('la del onClick ', data);
+
+      this.setState({ notes: data, content: '' });
+    } catch (ex) {
+      this.setState({ err: ex });
+    }
   };
+
+  onChange = event => {
+    this.setState({ content: event.target.value });
+  };
+
   async componentDidMount() {
-    await this.getData();
+    try {
+      const notes = await fetchNotes();
+
+      this.setState({
+        notes
+      });
+    } catch (ex) {
+      this.setState({
+        err: ex.message
+      });
+    }
   }
 
   render() {
+    const { notes, err, content } = this.state;
+    console.log('err ', err);
+    console.log('data', notes);
+    console.log('on change', content);
     return (
       <React.Fragment>
         <div className="search">
-          <input className="input" />
-          <button className="btn">agregar</button>
+          <input value={content} onChange={this.onChange} className="input" />
+          <button onClick={this.onClick} className="btn">
+            agregar
+          </button>
         </div>
-        <div>{}</div>
+        <div>
+          <ul>
+            {notes &&
+              notes.data.map(note => {
+                return <li key={note._id}>{note.content} </li>;
+              })}
+          </ul>
+        </div>
       </React.Fragment>
     );
   }
---
+++ frontend/src/utils/api.js
@@ -0,0 +1,18 @@
+import axios from './axios';
+
+export async function fetchNotes() {
+  try {
+    const data = await axios.get('/note');
+    return data;
+  } catch (ex) {
+    return ex;
+  }
+}
+
+export async function postNote(content) {
+  const data = await axios.post('/note', {
+    content
+  });
+
+  return data;
+}
